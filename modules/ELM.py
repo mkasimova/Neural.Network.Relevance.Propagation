5253a0782ad3b3edb9cecb99adacf233b9bb0b37
Oliver Fleetwood <oliver.fleetwood@gmail.com>
2018-10-26 11:49:56 +0200

    ELM and NN working

--- modules/ELM.py
+++ modules/ELM.py
@@ -9,15 +9,23 @@ import numpy as np
 
 logger = logging.getLogger("elm")
 
-def pseudo_inverse(x):
+
+def pseudo_inverse(x, alpha=None):
     # see eq 3.17 in bishop
     try:
-        inv= np.linalg.inv(np.matmul(x.T,x))
+        inner = np.matmul(x.T,x)
+        if alpha is not None:
+            tikonov_matrix = np.eye(x.shape[1])*alpha
+            inner += np.matmul(tikonov_matrix.T, tikonov_matrix)
+        inv= np.linalg.inv(inner)
         return np.matmul(inv, x.T)
     except np.linalg.linalg.LinAlgError as ex:
         #logger.exception(ex)
         logger.debug("Singular matrix")
         #Moore Penrose inverse rule, see paper on ELM
+        if alpha is not None:
+            tikonov_matrix = np.eye(x.shape[0])*alpha
+            inner += np.matmul(tikonov_matrix, tikonov_matrix.T)
         inv = np.linalg.inv(np.matmul(x, x.T))
         return np.matmul(x.T, inv)
 
@@ -37,80 +45,35 @@ def g_ELM(x, func_name):
     else:
         raise Exception("No such activation function {}".format(func_name))
 
-class MultiLayerELMClassifier(object):
-    def __init__(self, nlayers, L, activation_func):
-        self.nlayers = nlayers
-        self.L = L
+
+class SingleLayerELMClassifier(object):
+    def __init__(self, n_nodes=None, activation_func="hard_relu", alpha=1.):
+        self.n_nodes = n_nodes
         self.activation_func = activation_func
-        self.weights = []
-        self.biases = []
-        self.weights_E = []
+        self.coefs_ = None
+        self.intercepts_ = None
+        self.alpha = alpha #regularization constant
         
-    def train(self, x, t):
-        XE = x.T
+    def fit(self, x, t):
         (N, n) = x.shape
-        T = t.T
-        H, H_prev = None, None
-        W, b, WE = None, None, None
-        for l_idx in range(self.nlayers):
-           
-            if l_idx == 0:
-                #print"x, XE, n", x.shape, XE.shape, n)
-                #print"t, T", t.shape, T.shape)
-                #step 1
-                WE = random_matrix(self.L, n)
-                
-                #print"layer 1", self.W1.shape, self.b1.shape, self.W1E.shape)
-                #step 2
-                H = g_ELM(np.matmul(WE,XE), self.activation_func).T
-                #print"H, inv", H.shape, pseudo_inverse(H).shape)
-                #step 3
-                self.beta = np.matmul(pseudo_inverse(H), t)
-                #print"beta", beta.shape)
-            else:
-                #step 4
-                #print("H_prev", H_prev.shape)
-                H = np.matmul(t, pseudo_inverse(self.beta))
-                #print("H", H.shape)
-                #step 5
-                #append one column to include the bias at this layer
-                dummyvec = np.zeros((N,1)) + 1 
-                #print("dummy", dummyvec.shape)
-                HE = np.append(dummyvec, H_prev, axis=1) 
-                #print("HE", HE.shape)
-                WE = np.matmul(pseudo_inverse(HE),inv_g_ELM(H, self.activation_func))
-                #print("WE", WE.shape)
-                b = WE[:,0]
-                self.W2 = WE[:,1:]
-                #print"layer 2", self.W2.shape, self.b2.shape)
-                
-                #step 6
-                H = g_ELM(np.matmul(HE, WE), self.activation_func)
-                #print"H2", H2.shape)
-                
-                #step 7
-                self.beta = np.matmul(pseudo_inverse(H), t)
-                #print"beta_new", self.beta_new.shape)
-                
-            W = WE[1:, :]
-            b = WE[0, :]
-            self.weights.append(W)
-            self.weights_E.append(WE)
-            b = np.expand_dims(b, axis=0)
-            self.biases.append(b)
-            H_prev = H
+        if self.n_nodes is None:
+            self.n_nodes = n
+            logger.info("Automatically settings number of nodes in first layer to %s", self.n_nodes)
+        W1 = random_matrix(n, self.n_nodes)
+        b1 = random_matrix(1,self.n_nodes)
+        H = g_ELM(np.matmul(x, W1) + b1, self.activation_func)
+        W2 = np.matmul(pseudo_inverse(H, alpha=self.alpha),t)
+        self.coefs_ = [W1, W2]
+        self.intercepts_ = [b1, np.zeros((1,t.shape[1]))]
             
-    def predict(self,x):
-        #print"\nprediction")
-        res = None
-        for l_idx, W in enumerate(self.weights):
-            if l_idx == 0:
-                #due to the data format of X we've baked in the biases in to the first weight vector
-                H = g_ELM(np.matmul(self.weights_E[l_idx],x.T), func_name=self.activation_func).T
-                res = H
-            else:
-                res = np.matmul(res, W)
-                res += self.biases[l_idx]
-                res = g_ELM(res, func_name=self.activation_func)
-        res = np.matmul(res, self.beta)
-        return res
+    def predict(self, x):
+        H = g_ELM(np.matmul(x, self.coefs_[0]) + self.intercepts_[0], func_name=self.activation_func)
+        t = np.matmul(H, self.coefs_[1])
+        #print(t.shape)
+        for row_idx, row in enumerate(t):
+            #print(row_idx)
+            c_idx = row.argmax()
+            t[row_idx, :] = 0 
+            t[row_idx, c_idx] = 1
+            
+        return t

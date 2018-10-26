89d3b8f67db33b8be137680075aaff6a5b0faf92
anniewesterlund <34099005+anniewesterlund@users.noreply.github.com>
2018-10-26 12:20:25 +0200

    Add files via upload

--- modules/random_forest_features.py
+++ modules/random_forest_features.py
@@ -5,46 +5,25 @@ import shutil
 import time
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from scipy.spatial.distance import squareform
 
-class RF_feature_extract(object):
-	
-	n_iterations = 10
+import feature_extractor as FE
 
-	samples = []
-	labels = []
+class RF_feature_extract(FE.feature_extractor):
 	
-	def __init__(self,samples, labels, n_iterations=10):
-		
-		# Setting parameters
-		self.n_iterations = n_iterations
-		
-		# Reading data
-		self.samples = samples
-		self.labels = labels
+	def __init__(self,samples, labels, n_splits=10, scaling=True, n_estimators=20, njobs=4):
+		FE.feature_extractor.__init__(self, samples, labels, n_splits=n_splits, scaling=scaling)
+		self.n_estimators = n_estimators
+		self.njobs = njobs
 		return
 	
-	def train_RF(self,n_estimators=100,njobs=4):
+	def train(self, train_set, train_labels):
 		print('Training RF')
 		# Construct and train classifier
-		self.classifier = RandomForestClassifier(n_estimators=n_estimators, n_jobs=njobs)
-		self.classifier.fit(self.samples, self.labels)
-		return
-
-	def extract_features(self):
-		
-		for i_iter in range(self.n_iterations):
-			self.train_RF()
-			# Get feature importances
-			feature_importance = self.classifier.feature_importances_
-			summed_FI = np.sum(squareform(feature_importance),axis=1)
-			
-			if i_iter == 0:
-				summed_feats = np.zeros((self.n_iterations,summed_FI.shape[0]))
-				feats = np.zeros((self.n_iterations,feature_importance.shape[0]))
-			
-			summed_feats[i_iter,:] = summed_FI
-			feats[i_iter,:] = feature_importance
-		
-		return feats, summed_feats
+		classifier = RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=self.njobs)
+		classifier.fit(train_set, train_labels)
+		return classifier
+	
+	def get_feature_importance(self, classifier):
+		return classifier.feature_importances_
+	
 

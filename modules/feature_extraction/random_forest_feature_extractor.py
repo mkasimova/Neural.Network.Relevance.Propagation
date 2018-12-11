eb3870209fa0be48dfd14e3f3cae8eca44044a5f
Oliver Fleetwood <oliver.fleetwood@gmail.com>
2018-12-11 17:13:36 +0100

    Added controlled randomization to RF and default using all processors

--- modules/feature_extraction/random_forest_feature_extractor.py
+++ modules/feature_extraction/random_forest_feature_extractor.py
@@ -16,7 +16,7 @@ logger = logging.getLogger("RF featurizer")
 
 class RandomForestFeatureExtractor(FeatureExtractor):
 
-    def __init__(self, samples, cluster_indices, n_splits=10, n_iterations=10, scaling=True, filter_by_distance_cutoff=False, n_estimators=30, njobs=4, contact_cutoff=0.5, name="RF"):
+    def __init__(self, samples, cluster_indices, n_splits=10, n_iterations=10, scaling=True, filter_by_distance_cutoff=False, n_estimators=30, njobs=-1, contact_cutoff=0.5, randomize=True, name="RF"):
 
         FeatureExtractor.__init__(self, samples, cluster_indices, n_splits=n_splits, n_iterations=n_iterations, scaling=scaling, filter_by_distance_cutoff=filter_by_distance_cutoff, contact_cutoff=contact_cutoff, name=name)
         self.n_estimators = n_estimators
@@ -25,7 +25,9 @@ class RandomForestFeatureExtractor(FeatureExtractor):
 
     def train(self, train_set, train_labels):
         # Construct and train classifier
-        classifier = RandomForestClassifier(n_estimators=self.n_estimators, n_jobs=self.njobs)
+        classifier = RandomForestClassifier(n_estimators=self.n_estimators, 
+                                            n_jobs=self.njobs,
+                                            random_state=(None if self.randomize else 89274))
         classifier.fit(train_set, train_labels)
         return classifier
 
